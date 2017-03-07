84257ecee7582e287002c20ab9c89b7cc6c24bf3
Harikan Kangbid <MIN.DO@hotmail.com>
2017-03-07 11:09:57 +0700

    Initial Commit

---
+++ DemoThreeRingControl/DemoThreeRingControl.h
@@ -0,0 +1,19 @@
+//
+//  DemoThreeRingControl.h
+//  DemoThreeRingControl
+//
+//  Created by Harikan Kangbid on 3/7/2560 BE.
+//  Copyright © 2560 Harikan Kangbid. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for DemoThreeRingControl.
+FOUNDATION_EXPORT double DemoThreeRingControlVersionNumber;
+
+//! Project version string for DemoThreeRingControl.
+FOUNDATION_EXPORT const unsigned char DemoThreeRingControlVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DemoThreeRingControl/PublicHeader.h>
+
+
